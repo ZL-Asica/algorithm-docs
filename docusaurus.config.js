ac669b288a0205c00aad0acfa71f9effe758359c
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-10-27 01:34:45 -0500

    build: rename sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docs',
             position: 'left',
             label: '文档',
           },
